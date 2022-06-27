ee2fd7351f57b34d4c5b74a6d9599fe786e73964
Priyal2807 <priyalsobti@gmail.com>
2022-06-27 19:31:37 +0530

    updating index.js as per the requirements

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
+const upload = multer({dest:'uploads/'})
 require('dotenv').config()
 
 var app = express();
@@ -10,6 +12,13 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile') , (req, res, next) => {
+
+    res.send({
+        name: req.file.originalname, type: req.file.mimetype, size: req.file.size
+    })
+})
 
 
 
